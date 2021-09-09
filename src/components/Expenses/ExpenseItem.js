89ca62aad7bb0ab5d6b2b6b7cb8948ebbd27d65f
Dylan Hoffman <jkioicefire@gmail.com>
2021-09-09 05:32:37 -0500

    added onclick for expense item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,11 +6,10 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
-    let title = props.title;
+    const [title, setTitle] = useState(props.title);
+
 
     const handleClick = () => {
-
-        title="updated";
         console.log(title);
     }
 
@@ -18,7 +17,7 @@ const ExpenseItem = (props) => {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
         <button onClick={handleClick} > Change Title</button>
